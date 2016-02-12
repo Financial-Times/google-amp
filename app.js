c130253f6920b7203fc13074d8e992b02b6c2126
Matt Brennan <mattyb1000@gmail.com>
2016-02-12 11:41:37 +0000

    no api key

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const port = process.env.PORT || 5000;
 const app = express();
-const apiKeyMware = require('./server/middleware/keyAuth'); 
-
-app.use(apiKeyMware);
 
 // The search interface route
 app.get('/', require('./server/controllers/rss.js'));
@@ -11,5 +8,4 @@ app.get('/', require('./server/controllers/rss.js'));
 app.get('/content/:uuid', require('./server/controllers/amp-page.js'));
 app.get('/api/:uuid', require('./server/controllers/jsonItem.js'));
 
-app.listen(port);
-console.log('Up and running on port', port);
+app.listen(port, () => console.log('Up and running on port', port));
