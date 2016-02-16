4577cf9084a0e33fd0b8662d1e9a9947aaf1fb72
George Crawford <george.crawford@ft.com>
2016-02-16 17:15:02 +0000

    Add basic tracking

--- server/controllers/analytics-proxy.js
+++ server/controllers/analytics-proxy.js
@@ -4,12 +4,18 @@ const fs = require('fs-promise');
 
 module.exports = (req, res, next) => {
 
-	// console.log(req.method, JSON.stringify(req.query || {}));
+	fs.writeFile(`${path.resolve('analytics.log')}`, `${new Date().toJSON()} ${req.method}: ${JSON.stringify(req.query || {})}\n`);
 
-	fs.writeFile(`${path.resolve('analytics.log')}`, `${new Date().toJSON()} ${req.headers.accept === 'application/json' ? 'JSON' : 'GIF'} ${req.method}: ${JSON.stringify(req.query || {})}\n`);
-
-	// Switch between JSON and image/gif depending on what the client asked for
-	if (req.headers.accept === 'application/json') {
+	// Switch between JSON and image/gif depending on what the client sent
+	//
+	// https://github.com/ampproject/amphtml/blob/master/extensions/amp-analytics/amp-analytics.md
+	// 		- beacon Indicates navigator.sendBeacon can be used to transmit
+	// 			the request. This will send a POST request, with credentials, and an empty body.
+	// 		- xhrpost Indicates XMLHttpRequest can be used to transmit the request.
+	// 			This will send a POST request, with credentials, and an empty body.
+	//		- image Indicates the request can be sent by generating an
+	//			Image tag. This will send a GET request.
+	if (req.method === 'POST') {
 		res.setHeader('Content-Type', 'application/json');
 		res.status(202).json({'status': 'ok'});
 	} else {
