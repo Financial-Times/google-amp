aca47f8681138f49400583e240571a1eeeb6115d
Matt Brennan <mattyb1000@gmail.com>
2016-02-17 17:18:31 +0000

    allow content older than 30 days to be rendered

---
+++ server/lib/article-is-free.js
@@ -0,0 +1,17 @@
+const THIRTY_DAYS = 30 * 24 * 60 * 60 * 1000;
+
+module.exports = (article, options) => {
+	if(options.dev) {
+		return true;
+	}
+
+	if(/^http:\/\/www.ft.com\/cms\/s\/2/.test(article.webUrl)) {
+		return true;
+	}
+	console.log(new Date(), new Date(article.publishedDate), new Date() - new Date(article.publishedDate), THIRTY_DAYS);
+	if(new Date() - new Date(article.publishedDate) > THIRTY_DAYS) {
+		return true;
+	}
+
+	return false;
+};
