a749b487d1a977496b6acdfd5e549607f61c3983
Matt Brennan <mattyb1000@gmail.com>
2016-02-17 16:32:07 +0000

    Send /analytics/config.json in production mode

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ app.get('/api/:uuid', require('./server/controllers/jsonItem.js'));
 
 if(app.get('env') === 'development') {
 	app.all('/analytics', require('./server/controllers/analytics-proxy.js'));
-	app.all('/analytics/config.json', require('./server/controllers/analytics-config.js'));
 	app.use(require('errorhandler')());
 }
+
+app.all('/analytics/config.json', require('./server/controllers/analytics-config.js'));
 
 app.listen(port, () => console.log('Up and running on port', port));
