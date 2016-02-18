6f5ddca1b7cb1f2a0015f2e377ce4b73da06f77e
Matt Brennan <mattyb1000@gmail.com>
2016-02-18 14:37:32 +0000

    endpoint to set and clear cookie

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ app.use(cookieParser());
 app.get('/content/:uuid', require('./server/controllers/amp-page.js'));
 app.get('/api/:uuid', require('./server/controllers/jsonItem.js'));
 
+app.get('/_article_test', (req, res) => {
+	res.cookie('amp_article_test', '1');
+	res.sendStatus(204);
+});
+app.get('/_article_test/clear', (req, res) => {
+	res.clearCookie('amp_article_test');
+	res.sendStatus(204);
+});
+
 if(app.get('env') === 'development') {
 	app.all('/analytics', require('./server/controllers/analytics-proxy.js'));
 	app.use(require('errorhandler')());
