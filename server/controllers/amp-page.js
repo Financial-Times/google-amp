c4ff3f91efe7a7f9cd62ed2853c83113d189fc7a
George Crawford <george.crawford@ft.com>
2016-02-19 17:43:51 +0000

    Fix mock URLs

--- server/controllers/amp-page.js
+++ server/controllers/amp-page.js
@@ -11,11 +11,11 @@ module.exports = (req, res, next) => {
 		.then(apiResponse => apiResponse._source ? transformArticle(apiResponse._source) : Promise.reject(new errors.NotFound()))
 		.then(article => isFree(article, req) ? article : Promise.reject(new errors.NotFound()))
 		.then(data => {
-			const useMock = req.app.get('env') === 'production' || mockAuth;
+			const useMock = (req.app.get('env') === 'production') || mockAccessAuthentication;
 
-			data.AUTH_AUTHORIZATION_URL = useMock ? '//localhost:5000/amp-access-mock?type=access' : 'https://amp-access-svc-euwest1.memb.ft.com/amp-access/v1?';
-			data.AUTH_PINGBACK_URL      = useMock ? '//localhost:5000/amp-access-mock?type=pingback' : 'https://amp-access-svc-euwest1.memb.ft.com/amp-pingback/v1?';
-			data.AUTH_LOGIN_URL         = useMock ? '//localhost:5000/amp-access-mock?type=login' : 'https://amp-access-svc-euwest1.memb.ft.com/amp-login/v1?';
+			data.AUTH_AUTHORIZATION_URL = useMock ? '//localhost:5000/amp-access-mock?type=access&' : 'https://amp-access-svc-euwest1.memb.ft.com/amp-access/v1?';
+			data.AUTH_PINGBACK_URL      = useMock ? '//localhost:5000/amp-access-mock?type=pingback&' : 'https://amp-access-svc-euwest1.memb.ft.com/amp-pingback/v1?';
+			data.AUTH_LOGIN_URL         = useMock ? '//localhost:5000/amp-access-mock?type=login&' : 'https://amp-access-svc-euwest1.memb.ft.com/amp-login/v1?';
 			data.SOURCE_PORT            = (req.app.get('env') === 'production') ? '' : ':5000';
 
 			return data;
