5dde08e2ea3f50d99b6dc03be007f89d0fb5a0dc
George Crawford <george.crawford@ft.com>
2016-02-24 17:03:38 +0000

    Linting

--- server/controllers/access-mock.js
+++ server/controllers/access-mock.js
@@ -1,5 +1,4 @@
-module.exports = (req, res, next) => {
-
+module.exports = (req, res) => {
 	// CORS
 	res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, HEAD');
@@ -9,22 +8,26 @@ module.exports = (req, res, next) => {
 	res.setHeader('AMP-Access-Control-Allow-Source-Origin', req.query.__amp_source_origin);
 	res.setHeader('Access-Control-Expose-Headers', 'AMP-Access-Control-Allow-Source-Origin');
 
-	switch (req.query.type) {
-		case 'access':
-			res.setHeader('Content-Type', 'application/json');
-			res.status(202).json({
-				"access": true,
-				"debug": "access-mock granted access"
-			});
-			break;
-		case 'pingback':
-			res.setHeader('Content-Type', 'application/json');
-			res.status(202).json({});
-			break;
-		case 'login':
-			res.setHeader('Content-Type', 'application/json');
-			res.status(202).json({});
-			break;
+	switch(req.query.type) {
+	case 'access':
+		res.setHeader('Content-Type', 'application/json');
+		res.status(202).json({
+			access: true,
+			debug: 'access-mock granted access',
+		});
+		break;
+	case 'pingback':
+		res.setHeader('Content-Type', 'application/json');
+		res.status(202).json({});
+		break;
+	case 'login':
+		res.setHeader('Content-Type', 'application/json');
+		res.status(202).json({});
+		break;
+	default:
+		res.status(404).json({
+			error: `Unknown method: ${req.query.type}`,
+		});
+		break;
 	}
 };
-
