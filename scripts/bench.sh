d8636671f02f8745cd8d94b9d33218b6a52d22d2
Matt Brennan <mattyb1000@gmail.com>
2016-02-26 16:01:55 +0000

    benchmark script

---
+++ scripts/bench.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+. ./scripts/test-uuids.sh
+. ./scripts/env.sh
+
+set -x -e
+
+PORT=5001 node --use-strict app.js & > /dev/null
+PID=$!
+
+sleep 5
+
+TMP=$(mktemp urls-XXXX)
+for UUID in "${TEST_UUIDS[@]}"; do
+	echo "http://localhost:5001/content/$UUID"
+done > $TMP
+
+siege -f $TMP
+
+rm -f $TMP
+kill $PID
