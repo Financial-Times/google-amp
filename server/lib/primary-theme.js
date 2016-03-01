02076690c974c51bb0f51772230d854b027085e5
George Crawford <george.crawford@ft.com>
2016-03-01 14:24:51 +0000

    Link to primary theme in story packages

--- server/lib/primary-theme.js
+++ server/lib/primary-theme.js
@@ -1,16 +1,12 @@
-const fetch = require('node-fetch');
-
-const validateStreamUrl = (url) => fetch(url)
-	.then(res => res.status === 200);
+const getStreamUrl = require('./get-stream-url');
 
 module.exports = (article) => {
 	const primaryTheme = (article.metadata || []).filter(item => !!item.primary)[0];
 	if(!primaryTheme) return;
 
-	const streamUrl = `http://www.ft.com/stream/${primaryTheme.taxonomy}Id/${primaryTheme.idV1}`;
-	return validateStreamUrl(streamUrl)
-		.then(valid => {
-			if(!valid) return;
+	return getStreamUrl(primaryTheme)
+		.then(streamUrl => {
+			if(!streamUrl) return;
 
 			article.primaryTheme = {
 				url: streamUrl,
