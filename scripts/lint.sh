60853ecb2bda4fef987e2ca832acfb587966c744
Matt Brennan <mattyb1000@gmail.com>
2016-03-02 14:12:24 +0000

    fix grep syntax

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 {
-	git ls-files | egrep '\.(html|js|json|scss|sh|xsl)$' | grep -v {package,bower}.json | xargs lintspaces -n -d tabs -l 2
+	git ls-files | egrep '\.(html|js|json|scss|sh|xsl)$' | egrep -v '(package|bower).json' | xargs lintspaces -n -d tabs -l 2
 } && {
 	git ls-files | egrep '\.js$' | xargs eslint --fix
 }
