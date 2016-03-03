2a2ad011c79bd64490eb08035456a7473195cc4e
George Crawford <george.crawford@ft.com>
2016-03-03 11:34:14 +0000

    Fix ZLib issue in HEAD request

--- server/lib/get-stream-url.js
+++ server/lib/get-stream-url.js
@@ -3,6 +3,8 @@ const fetch = require('node-fetch');
 module.exports = (metadatum) => {
 	const streamUrl = `http://www.ft.com/stream/${metadatum.taxonomy}Id/${metadatum.idV1}`;
 
-	return fetch(streamUrl, {method: 'HEAD'})
+	// NB. Node 5 encounters a ZLib decompression issue if a HEAD request
+	// returns GZip content. See https://github.com/bitinn/node-fetch/issues/45.
+	return fetch(streamUrl, {method: 'HEAD', compress: false})
 		.then(res => res.status === 200 && streamUrl);
 };
