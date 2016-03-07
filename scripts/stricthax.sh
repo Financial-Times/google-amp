4d9fc5b9bbad92afcf071e898add03721fe7d944
Matt Brennan <mattyb1000@gmail.com>
2016-03-07 12:15:54 +0000

    add debug script and strict mode hack for o-date

---
+++ scripts/stricthax.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# this is horrible :(
+
+HAXFILES=(
+	node_modules/o-date/main.js
+)
+
+for f in "${HAXFILES[@]}"; do
+	echo '"use strict";' | cat - "$f" > "${f}-tmp"
+	mv "${f}-tmp" "$f"
+done
