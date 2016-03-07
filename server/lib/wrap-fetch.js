76d6d8ab7b8ddd21fc5cbc704642c588d62a9a76
Matt Brennan <mattyb1000@gmail.com>
2016-03-07 17:13:40 +0000

    lint

--- server/lib/wrap-fetch.js
+++ server/lib/wrap-fetch.js
@@ -1,10 +1,11 @@
-"use strict";
+'use strict';
 
-var log = [];
+const log = [];
 
-module.exports = (tag, fetch) => process.env.NODE_ENV !== 'instrument' ? fetch : function(url) {
-	let label = url.filter ? url.filter[1] : url;
-	let item = {tag, label, start: process.hrtime()};
+module.exports = (tag, fetch) => process.env.NODE_ENV !== 'instrument' ? fetch : function wrappedFetch(url) {
+	const label = url.filter ? url.filter[1] : url;
+	const item = {tag, label, start: process.hrtime()};
+
 	return fetch.apply(null, arguments).then(r => {
 		item.end = process.hrtime();
 		item.length = process.hrtime(item.start);
