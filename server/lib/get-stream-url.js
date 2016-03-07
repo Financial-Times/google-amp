7f91a8e3b7bd97d77bd4ad6b305467f55d4e6367
George Crawford <george.crawford@ft.com>
2016-03-07 20:12:19 +0000

    Trap 'maximum redirect' errors and assume 30x redirect can be used

--- server/lib/get-stream-url.js
+++ server/lib/get-stream-url.js
@@ -7,5 +7,14 @@ module.exports = (metadatum) => {
 	// NB. Node 5 encounters a ZLib decompression issue if a HEAD request
 	// returns GZip content. See https://github.com/bitinn/node-fetch/issues/45.
 	return fetch(streamUrl, {method: 'HEAD', compress: false, follow: 0})
-		.then(res => res.status !== 404 && streamUrl);
+	.catch(e => {
+		// Don't follow the redirect, but assume that if a redirect code is given
+		//  (see Fetch.prototype.isRedirect), the falcon redirect service has
+		//  found a corresponding URL on FT.com, and streamUrl can be served.
+		if(/maximum redirect reached/.test(e.message)) {
+			return streamUrl;
+		}
+
+		throw e;
+	});
 };
