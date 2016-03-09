7c526c1918624e942f0d747af7f3e8607c72ca4a
George Crawford <george.crawford@ft.com>
2016-03-09 15:19:52 +0000

    proper templating

--- server/lib/transform-slideshows.js
+++ server/lib/transform-slideshows.js
@@ -1,26 +1,39 @@
 'use strict';
 const cheerio = require('cheerio');
 const captionHeight = 50;
+const handlebars = require('handlebars');
+const cacheIf = require('@quarterto/cache-if');
+const fs = require('fs-promise');
+const path = require('path');
+const viewsPath = path.resolve('views');
 
-module.exports = function run(article) {
+const readTemplate = () => fs.readFile(`${viewsPath}/slideshow.html`, 'utf8').then(handlebars.compile);
+const getTemplate = precompiled => cacheIf(() => precompiled, readTemplate);
+
+module.exports = function run(article, options) {
 	const $ = cheerio.load(article.htmlBody, {decodeEntities: false});
 
-	$('ft-slideshow').map((index, el) => {
-		const uuid = el.attribs['data-uuid'];
-		const slideshow = article.slideshows[uuid];
+	return getTemplate(options.production)
+	.then(template => {
+		$('ft-slideshow').map((index, el) => {
+			const uuid = el.attribs['data-uuid'];
+			const slideshow = article.slideshows[uuid];
 
-		if(!slideshow) return $(el).remove();
+			if(!slideshow) return $(el).remove();
 
-		const images = slideshow.slides.map(slide => {
-			return `<div class="slideshow__slide"><amp-img class="slideshow__image" src="https://h2.ft.com/image/v1/images/raw/${slide.url}?source=amp&amp;fit=scale-down&amp;width=700" alt="${slide.alt}" layout="responsive" width="${slide.width}" height="${slide.height}"></amp-img><div class="slideshow__caption">${slide.caption}</div></div>`;
-		})
-		.join('');
+			const width = slideshow.slides.reduce((previous, current) => previous + current.width, 0) / slideshow.slides.length;
+			const height = slideshow.slides.reduce((previous, current) => previous + current.height, 0) / slideshow.slides.length;
 
-		const width = slideshow.slides.reduce((previous, current) => previous + current.width, 0) / slideshow.slides.length;
-		const height = slideshow.slides.reduce((previous, current) => previous + current.height, 0) / slideshow.slides.length;
+			const templated = template({
+				slides: slideshow.slides,
+				title: slideshow.title,
+				width,
+				height: height + captionHeight,
+			});
 
-		return $(el).replaceWith($(`<div class="slideshow"><div class="slideshow__title-wrapper"><amp-fit-text class="slideshow__title" layout="fill">${slideshow.title}</amp-fit-text></div><amp-carousel type="slides" layout="responsive" width="${width}" height="${height + captionHeight}">${images}</amp-carousel></div>`));
+			$(el).replaceWith($(templated));
+		});
+
+		article.htmlBody = $.html();
 	});
-
-	article.htmlBody = $.html();
 };
