0626f8b69916403c44565ecf43c3420806a69616
Matt Brennan <mattyb1000@gmail.com>
2016-03-16 11:57:01 +0000

    tag sentry errors with release

--- app.js
+++ app.js
@@ -47,7 +47,10 @@ if(app.get('env') === 'production') {
 	assertEnv(['SENTRY_DSN']);
 	ravenClient = new raven.Client(process.env.SENTRY_DSN);
 	ravenClient.setExtraContext({env: process.env});
-	ravenClient.setTagsContext({server_name: process.env.HEROKU_APP_NAME || os.hostname()});
+	ravenClient.setTagsContext({
+		server_name: process.env.HEROKU_APP_NAME || os.hostname(),
+		release: process.env.HEROKU_SLUG_COMMIT,
+	});
 	ravenClient.patchGlobal(() => process.exit(1));
 }
 
