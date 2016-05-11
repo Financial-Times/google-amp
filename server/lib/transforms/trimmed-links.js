7652e3ab7f69eee6e4d18cc9d93207055095e4dc
Matt Brennan <mattyb1000@gmail.com>
2016-05-11 17:22:29 +0100

    lint

--- server/lib/transforms/trimmed-links.js
+++ server/lib/transforms/trimmed-links.js
@@ -1,36 +1,36 @@
-"use strict";
+'use strict';
 
-var cheerio = require('cheerio');
+const cheerio = require('cheerio');
 
-module.exports = function ($) {
-	$('a').replaceWith(function(index, el) {
+module.exports = function trimmedLinks($) {
+	$('a').replaceWith((index, el) => {
 		el = cheerio(el);
-		var matches;
-		var contents = el.html()
-			.replace("\n", " ")
+		let matches;
+		let contents = el.html()
+			.replace('\n', ' ')
 			.replace('&#x2018;', '‘')
 			.replace('&#x2019;', '’')
 			.replace('&#x201C;', '“')
 			.replace('&#x201D;', '”');
 
-		var quoteReg = /^([“‘]) ?(.*) ?([”’])$/;
+		const quoteReg = /^([“‘]) ?(.*) ?([”’])$/;
 		matches = quoteReg.exec(contents);
-		if (matches) {
+		if(matches) {
 			el.html(matches[2]);
 			return matches[1] + $.html(el) + matches[3];
 		}
 
-		var trailingReg = /([ ,.;:]\s*)$/m;
-		var trailing = '';
-		var leadingReg = /^([ ,.;:]\s*)/m;
-		var leading = '';
+		const trailingReg = /([ ,.;:]\s*)$/m;
+		let trailing = '';
+		const leadingReg = /^([ ,.;:]\s*)/m;
+		let leading = '';
 		matches = trailingReg.exec(contents);
-		if (matches) {
+		if(matches) {
 			contents = contents.replace(trailingReg, '');
 			trailing = matches[0];
 		}
 		matches = leadingReg.exec(contents);
-		if (matches) {
+		if(matches) {
 			contents = contents.replace(leadingReg, '');
 			leading = matches[0];
 		}
