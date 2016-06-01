b8a80ec9dd1e4420fd39d1a5ddafbe4972c4137a
Matt Brennan <mattyb1000@gmail.com>
2016-06-01 14:57:54 +0100

    pants, it doesn't work. see if we can get something useful from the env

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -6,4 +6,6 @@ bower install
 
 post-sass --postCss autoprefixer --postCss cssnano
 
-npm version --no-git-tag $(git log HEAD --merges --pretty=oneline | grep -v "Merge branch 'master'" | wc -l).0.0
+env
+
+# npm version --no-git-tag $(git log HEAD --merges --pretty=oneline | grep -v "Merge branch 'master'" | wc -l).0.0
