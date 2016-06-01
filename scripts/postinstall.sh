75e4a107cab604af80fb5b62f4a67c27a0c82086
Matt Brennan <mattyb1000@gmail.com>
2016-06-01 14:57:09 +0100

    less verbosity

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -6,6 +6,4 @@ bower install
 
 post-sass --postCss autoprefixer --postCss cssnano
 
-version=$(git log HEAD --merges --pretty=oneline | grep -v "Merge branch 'master'" | wc -l).0.0
-echo "Inferred version $version"
-npm version --no-git-tag $version
+npm version --no-git-tag $(git log HEAD --merges --pretty=oneline | grep -v "Merge branch 'master'" | wc -l).0.0
