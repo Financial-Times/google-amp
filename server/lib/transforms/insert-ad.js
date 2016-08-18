fba986b7051399fb7cea9285edf8a7618f86064c
Matt Brennan <mattyb1000@gmail.com>
2016-08-18 15:32:00 +0100

    fix typo in option name

--- server/lib/transforms/insert-ad.js
+++ server/lib/transforms/insert-ad.js
@@ -4,7 +4,7 @@ const getViableParagraph = require('./get-viable-paragraph');
 const adMarkup = () => '<div class="ad-container"><amp-ad width="300" height="250" type="doubleclick"></amp-ad></div>';
 
 module.exports = function insertAd($, options) {
-	if(options.enabledAds) {
+	if(options.enableAds) {
 		getViableParagraph($).after(adMarkup(options.targeting));
 	}
 
