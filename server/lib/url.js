127b5c837cfe5187e7f6558049be8e47aa0b48db
George Crawford <george.crawford@ft.com>
2016-09-15 20:47:49 +0100

    linting

--- server/lib/url.js
+++ server/lib/url.js
@@ -6,27 +6,27 @@ const fetch = require('./wrap-fetch')(require('node-fetch'), {
 
 module.exports.canonical = article => {
 	switch(process.env.CANONICAL_URL_PHASE) {
-		case '0':
-			return article.webUrl;
-		case '1':
-			return `https://www.ft.com/content/${article.id}`;
-		case '2':
-			throw Error('Vanity URLs not yet supported');
-		default:
-			throw Error(`Unrecognised CANONICAL_URL_PHASE: ${JSON.stringify(process.env.CANONICAL_URL_PHASE)}`);
+	case '0':
+		return article.webUrl;
+	case '1':
+		return `https://www.ft.com/content/${article.id}`;
+	case '2':
+		throw Error('Vanity URLs not yet supported');
+	default:
+		throw Error(`Unrecognised CANONICAL_URL_PHASE: ${JSON.stringify(process.env.CANONICAL_URL_PHASE)}`);
 	}
 };
 
 module.exports.external = uuid => {
 	switch(process.env.CANONICAL_URL_PHASE) {
-		case '0':
-			return `http://www.ft.com/content/${uuid}`;
-		case '1':
-			return `https://www.ft.com/content/${uuid}`;
-		case '2':
-			throw Error('Vanity URLs not yet supported');
-		default:
-			throw Error(`Unrecognised CANONICAL_URL_PHASE: ${JSON.stringify(process.env.CANONICAL_URL_PHASE)}`);
+	case '0':
+		return `http://www.ft.com/content/${uuid}`;
+	case '1':
+		return `https://www.ft.com/content/${uuid}`;
+	case '2':
+		throw Error('Vanity URLs not yet supported');
+	default:
+		throw Error(`Unrecognised CANONICAL_URL_PHASE: ${JSON.stringify(process.env.CANONICAL_URL_PHASE)}`);
 	}
 };
 
