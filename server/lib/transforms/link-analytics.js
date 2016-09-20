d4f12d198fc0f362105b632c6e8550e2906a0ff6
George Crawford <george.crawford@ft.com>
2016-09-20 17:32:20 +0100

    Better encoding for data attributes

--- server/lib/transforms/link-analytics.js
+++ server/lib/transforms/link-analytics.js
@@ -1,13 +1,20 @@
 'use strict';
 
+const sanitise = text => text.replace(/[^\w ]/g, '');
+
 module.exports = function trimmedLinks($) {
 	$('a').each((i, el) => {
 		const $el = $(el);
 		const isRelatedBox = !!$el.parents('[data-trackable="related-box"]').length;
-		const text = $el.text();
+
+		// Ensure text doesn't contain HTML chars
+		const text = sanitise($el.text());
+
+		// Ensure URLs don't break out of data attribute
+		const href = $el.attr('href').replace('"', '%22');
 
 		if(!$el.attr('data-vars-link-destination')) {
-			$el.attr('data-vars-link-destination', $el.attr('href'));
+			$el.attr('data-vars-link-destination', href);
 		}
 
 		if(!$el.attr('data-vars-link-type')) {
