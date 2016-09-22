665252fae4a42156f5d4f0035e2b7340d39e4e81
Matt Brennan <mattyb1000@gmail.com>
2016-09-22 13:25:03 +0100

    request to the barrier guru

---
+++ server/controllers/products.js
@@ -0,0 +1,18 @@
+'use strict';
+const fetch = require('../lib/wrap-fetch')(require('node-fetch'), {
+	tag: 'barrier-guru',
+});
+
+const {json} = require('fetchres');
+const apiKey = process.env.BARRIER_GURU_API_KEY;
+
+module.exports = (req, res) =>
+	fetch('https://barrier-guru.ft.com/individual', {
+		headers: {
+			'x-api-key': apiKey,
+		},
+	})
+	.then(json)
+	.then(barrier => {
+		res.send(barrier);
+	});
