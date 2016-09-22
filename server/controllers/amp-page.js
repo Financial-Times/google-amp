5be0553d977a301559a6387ffbd1d8314f4b334a
Matt Brennan <mattyb1000@gmail.com>
2016-09-22 13:53:16 +0100

    behind a flag

--- server/controllers/amp-page.js
+++ server/controllers/amp-page.js
@@ -128,6 +128,7 @@ module.exports = (req, res, next) => {
 		enableSidebarMenu: (process.env.ENABLE_SIDEBAR_MENU === 'true'),
 		enableSocialShare: (process.env.ENABLE_SOCIAL_SHARE === 'true'),
 		enableAds: (process.env.ENABLE_ADS === 'true'),
+		enableBarrier: (process.env.ENABLE_BARRIER === 'true'),
 		uuid: req.params.uuid,
 		analyticsConfig: JSON.stringify(analytics.getJson({req, uuid: req.params.uuid})),
 	})
