69eead2470205a48a8fc96481cad9e6493db8812
Matt Brennan <mattyb1000@gmail.com>
2016-09-29 17:13:09 +0100

    fix new-style emoticon urls also

--- server/lib/transforms/fix-emoticons.js
+++ server/lib/transforms/fix-emoticons.js
@@ -3,7 +3,13 @@
 module.exports = $ => {
 	$('amp-img[src]').each((i, el) => {
 		const $el = $(el);
-		$el.attr('src', $el.attr('src').replace('assanka_web_chat', 'wp-plugin-ft-web-chat'));
+		$el.attr('src', $el.attr('src')
+			.replace(
+				/^(https:\/\/h2.ft.com\/image\/v1\/images\/raw\/)\/wp-content/,
+				'$1https://ftalphaville.ft.com/wp-content'
+			)
+			.replace('assanka_web_chat', 'wp-plugin-ft-web-chat')
+		);
 	});
 
 	return $;
