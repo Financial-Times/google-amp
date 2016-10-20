60cbb02ec3d920bdac281d46e5e8b61ff0c50b52
George Crawford <george.crawford@ft.com>
2016-10-20 13:02:05 +0100

    Add further required headers for Ammit segmentation

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -13,15 +13,27 @@ module.exports = (req, res, next) => {
 	const allocationId = req.get('ft-allocation-id') || req.cookies.FTAllocation;
 	const sessionId = req.get('ft-session-id') || req.cookies.FTSession;
 	const countryCode = req.get('country-code');
+	const countryCodeTwoLetters = req.get('country-code-two-letters');
+	const continentCode = req.get('continent_code');
+	const referer = req.get('referer');
+	const userAgent = req.get('user-agent');
 
 	res.vary('ft-allocation-id');
 	res.vary('ft-session-id');
 	res.vary('country-code');
+	res.vary('country-code-two-letters');
+	res.vary('continent_code');
+	res.vary('referer');
+	res.vary('user-agent');
 
 	module.exports.getProducts({
 		allocationId,
 		sessionId,
 		countryCode,
+		countryCodeTwoLetters,
+		continentCode,
+		referer,
+		userAgent,
 	}).then(({items, allocation}) => {
 		if(!allocationId && allocation) {
 			res.cookie('FTAllocation', allocation, {
@@ -34,7 +46,7 @@ module.exports = (req, res, next) => {
 	}).catch(next);
 };
 
-module.exports.getProducts = ({allocationId, sessionId, countryCode}) =>
+module.exports.getProducts = ({allocationId, sessionId, countryCode, countryCodeTwoLetters, continentCode, referer, userAgent}) =>
 	ammit({allocationId, sessionId})
 	.then(({abVars, allocation}) => fetch('https://barrier-guru.ft.com/individual', {
 		headers: {
