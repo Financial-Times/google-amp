56dc3716ee6018c6949ef9f3f5267081666364f4
Matt Brennan <mattyb1000@gmail.com>
2016-10-31 12:04:04 +0000

    traverse into media queries, but not keyframes. almost at parity with giakki/uncss

--- server/lib/transforms/uncss.js
+++ server/lib/transforms/uncss.js
@@ -3,13 +3,23 @@
 const cheerio = require('cheerio');
 const postcss = require('postcss');
 
+const maybe = fn => {
+	try {
+		return fn();
+	} catch(e) {
+		return false;
+	}
+};
+
 module.exports = postcss.plugin('uncss', ({html}) => {
-	const $ = cheerio.load(`<body>${html}</body>`);
-	console.log($('body').length);
-	return css => css.each(rule => {
-		if(rule.selector && !rule.selector.includes(':') && !$(rule.selector).length) {
-			console.log(`removing ${rule.selector}`);
-			rule.remove();
+	const $ = cheerio.load(`<html><body>${html}</body></html>`);
+	return css => css.walkRules(rule => {
+		if(rule.parent.name === 'keyframes') return;
+		if(rule.selector && !rule.selector.match(/:(?:not)/)) {
+			const selector = rule.selector.replace(/::?\w+/g, '');
+			if(maybe(() => $(selector).length === 0)) {
+				rule.remove();
+			}
 		}
 	});
 });
