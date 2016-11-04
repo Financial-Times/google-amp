61da61f56d85b5b9e092416e12d419dc4a4258ac
Matt Brennan <mattyb1000@gmail.com>
2016-11-04 13:27:28 +0000

    split heroku running stuff into modules

--- app.js
+++ app.js
@@ -1,29 +1,11 @@
 'use strict';
 
-const os = require('os');
-const v8 = require('v8');
-const throng = require('throng');
+const herokuV8Memory = require('@quarterto/heroku-v8-memory');
+const herokuCluster = require('@quarterto/heroku-cluster');
+const app = require('./server');
 
-if(process.env.WEB_MEMORY) {
-	const gcMemory = Math.floor(parseInt(process.env.WEB_MEMORY, 10) * 4 / 5);
-	v8.setFlagsFromString(`--max_old_space_size=${gcMemory}`);
-}
-
-const port = process.env.PORT || 5000;
-
-const workers = process.env.WEB_CONCURRENCY ?
-	parseInt(process.env.WEB_CONCURRENCY, 10) :
-	os.cpus().length;
-
-throng({
-	workers,
-	master() {
-		console.log(`Starting ${workers} workers`);
-	},
-
-	start() {
-		const app = require('./server');
-
-		app.listen(port, () => console.log(`Worker ${process.pid} listening on port ${port}`));
-		},
+herokuV8Memory();
+herokuCluster({
+	defaultPort: 5000,
+	app,
 });
