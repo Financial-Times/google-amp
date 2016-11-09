adb5aadb04e93b936ed9bd0d25efb3b71a396744
Matt Brennan <mattyb1000@gmail.com>
2016-11-09 16:13:37 +0000

    set old_space_size not at runtime, add a couple more heroku-recommended flags

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 'use strict';
 
-const herokuV8Memory = require('@quarterto/heroku-v8-memory');
 const herokuCluster = require('@quarterto/heroku-cluster');
 const app = require('./server');
 
-herokuV8Memory();
 herokuCluster({
 	defaultPort: 5000,
 	app,
---
+++ scripts/memory.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const herokuMemoryLimit = require('@quarterto/heroku-memory-limit');
+
+console.log(herokuMemoryLimit());
