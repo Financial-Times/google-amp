8613d374ad832c8236948b6de0a90296e453bad3
Matt Brennan <mattyb1000@gmail.com>
2016-11-11 13:08:03 +0000

    tune limit, print flags to verify

--- scripts/nodeargs.js
+++ scripts/nodeargs.js
@@ -4,12 +4,14 @@
 
 const herokuMemoryLimit = require('@quarterto/heroku-memory-limit');
 
-const limit = Math.floor(herokuMemoryLimit() / 2);
-
-console.log([
+const limit = herokuMemoryLimit(18 / 32);
+const flags = [
 	'--optimize_for_size',
 	'--gc_interval=100',
 	'--max_semi_space_size=1',
 	`--max_old_space_size=${limit}`,
 	`--max_executable_size=${limit}`,
-].join(' '));
+].join(' ');
+
+console.error(`Node flags: ${flags}`);
+console.log(flags);
