51858019b4726e12ab7c3bf24f0d306c4f78929d
Matt Brennan <mattyb1000@gmail.com>
2016-11-11 11:40:40 +0000

    more, and more generic, node options

--- scripts/memory.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-'use strict';
-
-const herokuMemoryLimit = require('@quarterto/heroku-memory-limit');
-
-console.log(herokuMemoryLimit());
---
+++ scripts/nodeargs.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const herokuMemoryLimit = require('@quarterto/heroku-memory-limit');
+
+const limit = herokuMemoryLimit();
+
+console.log([
+	'--optimize_for_size',
+	'--gc_interval=100',
+	'--max_semi_space_size=1',
+	`--max_old_space_size=${limit}`,
+	`--max_executable_size=${limit}`,
+].join(' '));
