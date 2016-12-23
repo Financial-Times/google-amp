a69c198ed54ed34cba73f1695be8b500d4ad286e
Matt Brennan <matt@153.io>
2016-12-23 11:44:35 +0000

    add IG transform test

---
+++ test/transform/interactive-graphics.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const {expect} = require('../../test-utils/chai');
+const xslt = require('../../server/lib/article-xslt');
+
+describe('interactive graphics transform', () => {
+	it('should transform to iframe if href is https', async () => {
+		expect(
+			await xslt('<a data-asset-type="interactive-graphic" href="https://ig.ft.com/foo" data-width="300" data-height="200">Graphic</a>')
+		).dom.to.equal(`<amp-iframe
+			src="https://ig.ft.com/foo"
+			sandbox="allow-scripts allow-same-origin"
+			layout="responsive"
+			frameborder="0"
+			width="300" height="200">
+		</amp-iframe>`);
+	});
+
+	it('should strip if href is not https', async () => {
+		expect(
+			await xslt('<a data-asset-type="interactive-graphic" href="http://ig.ft.com/foo" data-width="300" data-height="200">Graphic</a>')
+		).dom.to.equal('');
+	});
+});
