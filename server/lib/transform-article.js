35b3c7ccb19c79c2c650f826ac13d3e5ca332e1f
Matt Brennan <matt@153.io>
2017-01-03 15:46:56 +0000

    split out body bits of transform-article

--- server/lib/transform-article.js
+++ server/lib/transform-article.js
@@ -1,30 +1,12 @@
 'use strict';
 
-const articleXsltTransform = require('./article-xslt');
-const cheerioTransform = require('./cheerio-transform');
 const dateTransform = require('./article-date');
 const summaryTransform = require('./article-summary');
 const schemaHeadlineTransform = require('./article-headline');
 const extractMainImage = require('./transforms/extract-main-image');
+const transformBody = require('./transform-body');
 
-function transformArticleBody(article, options) {
-	const xsltParams = {
-		renderTOC: 0,
-		brightcoveAccountId: process.env.BRIGHTCOVE_ACCOUNT_ID,
-
-		// See: https://github.com/ampproject/amphtml/blob/master/extensions
-		// /amp-brightcove/amp-brightcove.md#player-configuration
-		// NB: Next don't use the native Brightcove player, so don't use this param.
-		// Default seems fine.
-		// brightcovePlayerId: process.env.BRIGHTCOVE_PLAYER_ID
-		brightcovePlayerId: 'default',
-	};
-
-	return articleXsltTransform(article.bodyXML, 'main', xsltParams)
-		.then(articleBody => cheerioTransform(articleBody, options));
-}
-
-module.exports = (contentItem, options) => transformArticleBody(contentItem, options)
+module.exports = (contentItem, options) => transformBody(contentItem.bodyXML, options)
 	.then(transformed$ => {
 		contentItem.mainImageHtml = extractMainImage(transformed$);
 		contentItem.htmlBody = transformed$.html();
---
+++ server/lib/transform-body.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const cheerioTransform = require('./cheerio-transform');
+const articleXsltTransform = require('./article-xslt');
+
+module.exports = (body, options) => {
+	const xsltParams = {
+		renderTOC: 0,
+		brightcoveAccountId: process.env.BRIGHTCOVE_ACCOUNT_ID,
+
+		// See: https://github.com/ampproject/amphtml/blob/master/extensions
+		// /amp-brightcove/amp-brightcove.md#player-configuration
+		// NB: Next don't use the native Brightcove player, so don't use this param.
+		// Default seems fine.
+		// brightcovePlayerId: process.env.BRIGHTCOVE_PLAYER_ID
+		brightcovePlayerId: 'default',
+	};
+
+	return articleXsltTransform(body, 'main', xsltParams)
+		.then(articleBody => cheerioTransform(articleBody, options));
+};
