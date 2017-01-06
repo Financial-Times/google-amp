26ee18d06bfb7b1541330f7ba7e280afef869c2f
Matt Brennan <matt@153.io>
2017-01-06 11:26:39 +0000

    port link analytics transform to cmm

--- server/lib/xml-transforms/link-analytics.js
+++ server/lib/xml-transforms/link-analytics.js
@@ -1,30 +1,32 @@
 'use strict';
 
+const match = require('@quarterto/cheerio-match-multiple');
+
 const sanitise = text => text.replace(/[^\w ]/g, '');
 
-module.exports = function trimmedLinks($) {
-	$('a').each((i, el) => {
-		const $el = $(el);
-		const isRelatedBox = !!$el.parents('[data-trackable="related-box"]').length;
+module.exports = match({
+	'[data-trackable="related-box"] a'(el) {
+		this.a(el);
+		el.attr('data-vars-link-type', 'related-box');
+	},
 
+	'a'(el) {
 		// Ensure text doesn't contain HTML chars
-		const text = sanitise($el.text());
+		const text = sanitise(el.text());
 
 		// Ensure URLs don't break out of data attribute
-		const href = $el.attr('href').replace('"', '%22');
+		const href = el.attr('href').replace('"', '%22');
 
-		if(!$el.attr('data-vars-link-destination')) {
-			$el.attr('data-vars-link-destination', href);
+		if(!el.attr('data-vars-link-destination')) {
+			el.attr('data-vars-link-destination', href);
 		}
 
-		if(!$el.attr('data-vars-link-type')) {
-			$el.attr('data-vars-link-type', (isRelatedBox ? 'related-box' : 'inline'));
+		if(!el.attr('data-vars-link-type')) {
+			el.attr('data-vars-link-type', 'inline');
 		}
 
-		if(!$el.attr('data-vars-link-text') && text) {
-			$el.attr('data-vars-link-text', text);
+		if(!el.attr('data-vars-link-text') && text) {
+			el.attr('data-vars-link-text', text);
 		}
-	});
-
-	return $;
-};
+	},
+});
