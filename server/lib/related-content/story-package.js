64ef2e15bf616649eba290421580be99f65a536d
Matt Brennan <matt@153.io>
2017-09-12 15:47:04 +0100

    fix relatedcontent

--- server/lib/related-content/story-package.js
+++ server/lib/related-content/story-package.js
@@ -5,23 +5,18 @@ const dateTransform = require('../transforms/extra/date');
 const sanitizeImage = require('../sanitize-image');
 const url = require('../url');
 
-const formatRelatedContent = (options, item) => {
-	const primaryTheme = (item.metadata || []).filter(metadatum => !!metadatum.primary)[0];
-
-	return url.stream(primaryTheme, options)
-		.then(streamUrl => ({
-			date: dateTransform(item.publishedDate, {classname: 'related-content__date'}),
-			id: item.id,
-			url: url.external(item.id),
-			title: item.title,
-			image: sanitizeImage(item.mainImage),
-			summary: item.standfirst,
-			theme: {
-				url: streamUrl,
-				name: primaryTheme.prefLabel,
-			},
-		}));
-};
+const formatRelatedContent = (options, item) => ({
+	date: dateTransform(item.publishedDate, {classname: 'related-content__date'}),
+	id: item.id,
+	url: url.external(item.id),
+	title: item.title,
+	image: sanitizeImage(item.mainImage),
+	summary: item.standfirst,
+	theme: {
+		url: item.displayConcept.url,
+		name: item.displayConcept.prefLabel,
+	},
+});
 
 const getRelated = (id, options) => nEsClient.get(id).catch(e => {
 	// Ignore 404 errors, for content not in ElasticSearch
@@ -40,7 +35,10 @@ const getRelated = (id, options) => nEsClient.get(id).catch(e => {
 });
 
 module.exports = (article, options) =>
-	Promise.all((article.curatedRelatedContent || article.storyPackage || []).map(related => getRelated(related.id, options)))
+	Promise.all(
+		(article.curatedRelatedContent || article.storyPackage || [])
+			.map(related => getRelated(related.id, options))
+	)
 	.then(related => related.filter(response => response))
 	.then(related => {
 		related.forEach(item => {
@@ -49,8 +47,5 @@ module.exports = (article, options) =>
 
 		return Promise.all(related.map(formatRelatedContent.bind(null, options)));
 	})
-	.then(related => {
-		article.relatedContent = related;
-		return article;
-	})
+	.then(relatedContent => Object.assign(article, {relatedContent}))
 	.catch(() => {});
