814eb1525d27e44ac5bf191dc05e6330882d87ed
Matt Brennan <matt@153.io>
2018-01-10 11:12:45 +0000

    Use kitchenus sinkus as non-prod redirect article

--- server/controllers/redirect.js
+++ server/controllers/redirect.js
@@ -2,7 +2,7 @@
 
 module.exports = (req, res) => {
 	const isStaging = req.app.get('env') === 'staging';
-	let url = '/content/6e4accd6-9260-11e6-8df8-d3778b55a923';
+	let url = '/content/146da558-4dee-11e3-8fa5-00144feabdc0';
 
 	if(isStaging) {
 		const host = req.get('host');
