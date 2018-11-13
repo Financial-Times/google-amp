1f8d62cf4e6559ef7abde22670fc71e7831b0618
Carles Andres <carlos.andres.c@gmail.com>
2018-11-13 15:03:16 +0000

    Add missing "pos" parameter to ad requests

--- server/lib/transforms/html/insert-ad.js
+++ server/lib/transforms/html/insert-ad.js
@@ -2,21 +2,22 @@
 
 const getViableParagraph = require('../utils/get-viable-paragraph');
 
-const adMarkup = (uuid, ftConsentCookie) => {
+const adMarkup = (uuid, ftConsentCookie, adPos) => {
 	let rtcConfig = '';
 	if(ftConsentCookie && ftConsentCookie.indexOf('behaviouraladsOnsite:on') !== -1) {
 		rtcConfig = `'{"urls":["https://ads-api.ft.com/v1/content/${uuid}?amp=true","https://ads-api.ft.com/v1/user?amp=true", "https://cdn.krxd.net/userdata/v2/amp/HINzStCn?segments_key=ksg&kuid_key=kuid"]}'`;
 	}	else {
 		rtcConfig = `'{"urls":["https://ads-api.ft.com/v1/content/${uuid}?amp=true","https://ads-api.ft.com/v1/user?amp=true"]}'`;
 	}
-	return `<div class="ad-container"><amp-ad width="300"	height="250" type="doubleclick"	data-slot="/5887/ft.com/distributed.content/amp.ft" rtc-config=${rtcConfig}></amp-ad></div>`;
+	const json = `{"targeting":{"pos":"${adPos}"}}`;
+	return `<div class="ad-container"><amp-ad width="300"	height="250" type="doubleclick"	data-slot="/5887/ft.com/distributed.content/amp.ft" json=${json} rtc-config=${rtcConfig}></amp-ad></div>`;
 };
 
 module.exports = function insertAd($, options) {
 	if(options.enableAds) {
 		const para = getViableParagraph($);
 		if(para) {
-			para.after(adMarkup(options.uuid, options.ftConsentCookie));
+			para.after(adMarkup(options.uuid, options.ftConsentCookie, 'mid'));
 		}
 	}
 
