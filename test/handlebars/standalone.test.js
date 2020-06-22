dd9b87fc638d2673d3d27f584fd993558f6d8cd6
estefania morton <estefaniamorton@estefanias-MacBook-Pro.local>
2020-06-22 17:04:14 -0500

    added test partials for hbs testing 🐿 v2.13.0

--- test/handlebars/standalone.test.js
+++ test/handlebars/standalone.test.js
@@ -19,7 +19,7 @@ describe('standalone', function () {
 
 				return instance.render(path.resolve(__dirname, '../fixtures/app/views/main.html'), {title: 'FT'})
 					.then(function (html) {
-						expect(html).to.match(/<h1>FT[\s\S]*<h3>I am partial of dep 2[\s\S]*<h3>I am partial of dep 2[\s\S]*<p>Concat onetwothree[\s\S]*/);
+						expect(html).to.match(/<h1>FT[\s\S]*<h3>I am partial of dep 1[\s\S]*<h3>I am partial of dep 2[\s\S]*<p>Concat onetwothree[\s\S]*/);
 						done();
 					});
 			})
